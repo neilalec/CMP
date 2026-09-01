f798e4a2775fcbe76d81bd591e6a6de7b225a99f
neilalec <neilalec.massella@gmail.com>
2026-09-01 15:47:49 +0100

    updated about section

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -5,7 +5,7 @@ import { useQueueStore } from '../stores/queueStore';
 
 const queueStore = useQueueStore();
 const serverName = '4K War Server';
-const serverStatus = computed(() => queueStore.activeLobbyCount > 0 ? 'in use' : 'available');
+const serverStatus = computed(() => queueStore.activeLobbyCount > 0 ? 'being played on' : 'not being played on');
 </script>
 
 <template>
@@ -18,36 +18,41 @@ const serverStatus = computed(() => queueStore.activeLobbyCount > 0 ? 'in use' :
         <section class="static-section">
           <h2 class="static-section-title is-large">How it Works</h2>
           <p>
-            Join a queue as solo or part of a group, accept the match when queue is filled, then you'll move to a lobby to map vote and receive server info. 
+            Join a queue as solo or part of a group, accept the match when queue is filled, you'll then be moved to a lobby to map vote and receive server info. 
           </p>
-            <p>You'll then have to join the server through the Custom Browser in Squad, the server info will be within your lobby, once enough players have joined or after 10 minutes the match will start.
+            <p>Join the server through the Custom Browser in Squad, once enough players have joined or after 10 minutes the match will start.
           </p>
           <p>
-            You'll play one round on the layer, at the end of which the score will save to the results section of this app, and you'll be able to queue for another match if you wish.
+            All server commands will happen automatically, including map rotation, team balancing, and match ending.
+          </p>
+          <p>
+            You'll play two rounds on the layer, one for each faction, once the match ends your ELO will be adjusted based on the outcome.
+          </p>
+          <p>
+            You can then queue for another match if you want.
           </p>
         </section>
 
         <section class="static-section">
           <h2 class="static-section-title is-large">Purpose</h2>
           <p>
-            To bridge the gap between public games and competitive scrims.
-              Players can queue for a match on classic and competitive layers, with other like minded players who want to play in a more competitive setting. 
-              <br>Team sizes range from 20v20 to 46v46, which are the formats used in competitive scrims and tournaments.
+          Competitive matchmaking for Squad, similar to other competitive first person shooters.
+          <br>
+          Queue for and find a match on competitive layers that differ from vanilla layers, they perform better as they disable shadows, and allow more consistently competitive gameplay due to the linearity of AAS, as opposed to RAAS, Invasion etc.
+          <br>
+          <br>
+          If you are a new you are highly encouraged to queue up for a game and take part, you will learn a lot about Squads meta, which is largely overlooked in public games. 
+          This is not meant to be the same experience as a scrim or tournament match, as the only time you'll have to prep your team is within staging phase and you'll play with randoms like in pubs, except the players you group with.
+          <br>
+          <br>
+          Feedback and bug reports are highly appreciated, please use the <a class="static-link" :href="DISCORD_INVITE_URL" target="_blank" rel="noopener noreferrer">discord</a> for that.
+          <br>
+          This is a hobbyist project, so please be patient with the service :)
           </p>
-          <p>
-            If you are looking for a more challenging experience to Squad this will hopefully be your best option without having to organize your own server and players, such as in scrims.
-          </p>
-          <p>If you are a new player to competitive Squad you are highly encouraged to queue up for a game and take part, you will learn alot about Squads meta, which is largely overlooked in public games.
-            This is not meant to be the same experience as a scrim or tournament match, as the only time you'll have to prep your team is within staging phase and you'll play with randoms like in pubs, except the players you group with
-          </p>
-          <p>
-            Feedback and bug reports are highly appreciated please use the <a class="static-link" :href="DISCORD_INVITE_URL" target="_blank" rel="noopener noreferrer">discord</a> for that
-          </p>
-          <p>This is a hobbyist project, so please be patient with the service :)</p>
         </section>
 
         <p class="notice-tile">
-          We are currently using one singular server: <strong>{{ serverName }}</strong> and its currently <strong>{{ serverStatus }}</strong>.
+          We currently use one server, the <strong>{{ serverName }}</strong>, which is <strong>{{ serverStatus }}</strong> at the moment.
         </p>
       </div>
     </section>
